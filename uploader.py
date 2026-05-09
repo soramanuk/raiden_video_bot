10f529b0c0b5e9ec5bb2ea0de737584f67c2c863
soramanuk <kaduasoramanuk@gmail.com>
2026-05-09 19:04:46 +0700

    Add files via upload

--- uploader.py
+++ uploader.py
@@ -326,7 +326,7 @@ async def generate_thumbnail(
 
     Return True jika berhasil, False jika FFmpeg gagal.
     """
-import asyncio
+    import asyncio
     import imageio_ffmpeg as _iio_ffmpeg
     _FFMPEG = _iio_ffmpeg.get_ffmpeg_exe()
 
